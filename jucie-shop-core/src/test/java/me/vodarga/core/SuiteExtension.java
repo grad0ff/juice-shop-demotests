57d75374f45f41e728dde377f976842ac5ea033f
grad0ff <grad0ff@mail.ru>
2026-03-22 16:53:41 +0300

    Добавить расширение JUnit для однократного выполнения методов до/после всех тестов

---
+++ jucie-shop-core/src/test/java/me/vodarga/core/SuiteExtension.java
@@ -0,0 +1,27 @@
+package me.vodarga.core;
+
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
+
+public interface SuiteExtension extends BeforeAllCallback {
+
+  default void beforeSuite() {
+    throw new RuntimeException("Method beforeSuite() must be overridden");
+  }
+
+  default void afterSuite() {
+    throw new RuntimeException("Method afterSuite() must be overridden");
+  }
+
+  @Override
+  default void beforeAll(ExtensionContext context) {
+    context.getRoot()
+        .getStore(Namespace.GLOBAL)
+        .getOrComputeIfAbsent(getClass(), key -> {
+          beforeSuite();
+          return (AutoCloseable) this::afterSuite;
+        });
+  }
+
+}
