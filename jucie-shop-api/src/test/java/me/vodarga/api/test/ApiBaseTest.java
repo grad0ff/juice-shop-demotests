f2462bd1c93380f3508137800f9d3a1ff6c09aed
grad0ff <grad0ff@mail.ru>
2026-03-22 18:30:28 +0300

    Добавить базовый класс для API тестов

---
+++ jucie-shop-api/src/test/java/me/vodarga/api/test/ApiBaseTest.java
@@ -0,0 +1,9 @@
+package me.vodarga.api.test;
+
+import me.vodarga.api.extension.RestAssuredConfigExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(RestAssuredConfigExtension.class)
+public abstract class ApiBaseTest {
+
+}
