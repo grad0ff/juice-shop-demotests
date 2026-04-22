0f68a2408010df85926fcab7468901551756ebf9
grad0ff <grad0ff@mail.ru>
2026-04-22 23:20:43 +0300

    Перенести Extension в класс

--- juice-shop-api/src/test/java/me/vodarga/api/test/ApiBaseTest.java
+++ juice-shop-api/src/test/java/me/vodarga/api/test/ApiBaseTest.java
@@ -2,17 +2,20 @@ package me.vodarga.api.test;
 
 import static me.vodarga.core.allure.AllureLayer.API;
 
-import me.vodarga.api.junit.extension.RestAssuredConfigExtension;
+import me.vodarga.api.junit.extension.ConfigurerExtension;
+import me.vodarga.api.service.RestAssuredConfigurer;
 import me.vodarga.core.allure.Layer;
+import me.vodarga.core.junit.extension.SuiteExtension;
 import me.vodarga.core.test.BaseTest;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 @Layer(API)
-@ExtendWith(RestAssuredConfigExtension.class)
 public abstract class ApiBaseTest extends BaseTest {
 
+  @RegisterExtension
+  private static final SuiteExtension suiteExtension = new ConfigurerExtension(new RestAssuredConfigurer());
   protected final SoftAssertions softly = new SoftAssertions();
 
   @AfterEach
